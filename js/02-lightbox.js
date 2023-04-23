28d7509c30aac0d5f278718495d49dbd20cc46c5
Tarasdeveloper <tomahavki1@gmail.com>
2023-04-23 22:14:27 +0300

    task 2 finished

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+//  Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи. Используй готовый код из первого задания.
+//  Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs. Необходимо добавить ссылки на два файла: simple-lightbox.min.js и simple-lightbox.min.css.
+//  Инициализация библиотеки после того как элементы галереи созданы и добавлены в ul.gallery. Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
+//  Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из атрибута alt. Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map((item) => {
+    return `<li class="gallery__item">
+                <a class="gallery__link" href="${item.original}">
+                    <img
+                        class="gallery__image"
+                        src="${item.preview}" 
+                        alt="${item.description}"
+                    >
+                </a>
+            </li>`;
+  })
+  .join("");
+galleryList.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox(".gallery .gallery__link", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
